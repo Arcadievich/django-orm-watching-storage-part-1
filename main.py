b34429b9ed9205d6d69464108801f7ca15b580c1
Arcadievich <user_274@mail.ru>
2025-08-05 16:10:28 +0300

    Correction

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ from datacenter.models import Passcard  # noqa: E402
 
 
 def main():
+    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
     active_passcards = Passcard.objects.filter(is_active=True)
     print(f'Активных пропусков: {len(active_passcards)}')
 
 
 if __name__ == '__main__':
     main()
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
